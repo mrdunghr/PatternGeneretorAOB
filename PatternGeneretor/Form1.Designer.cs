cf94de68fa7b4f2f9ea92972d6e34e2a962ba602
Mr.Dunghr <mrdunghr@gmail.com>
2023-12-28 07:16:08 +0700

    DONE

---
+++ PatternGeneretor/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace PatternGeneretor
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(12, 12);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(367, 386);
+            textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(12, 491);
+            textBox2.Multiline = true;
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(367, 82);
+            textBox2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(123, 404);
+            button1.Name = "button1";
+            button1.Size = new Size(129, 58);
+            button1.TabIndex = 2;
+            button1.Text = "Generetor";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(389, 585);
+            Controls.Add(button1);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "GEN AOB FULL";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Button button1;
+    }
+}
---
+++ PatternGeneretor/Form1.cs
@@ -0,0 +1,57 @@
+﻿namespace PatternGeneretor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                textBox2.Text = GeneratePattern(textBox1.Text);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        string GeneratePattern(string input)
+        {
+            // đọc từng dòng
+            string[] lines = input.Split('\n', StringSplitOptions.RemoveEmptyEntries);
+
+            // list để lưu mẫu conver
+            List<string> convertedBytes = new List<string>();
+
+            foreach (string line in lines)
+            {
+                // chia thành từng phần và bỏ qua nội dung địa chỉ
+                string[] chunks = line.Split(" - ");
+
+                // nhận byte ở định dạng chuỗi và xóa ký hiệu dòng mới
+                string[] bytes = chunks[1].Replace("\r", "").Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                // lặp qua byte và thay thế các địa chỉ tiềm năng bằng ký tự đại diện
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    if (bytes[i].Length == 8) // giả sử địa chỉ là 8 ký tự
+                        convertedBytes.Add("?? ?? ?? ??"); // ký tự đại diện
+                    else if (bytes[i] == "00") // loại bỏ các giá trị 00
+                        convertedBytes.Add("??");
+                    else // chia các byte cuối cùng thành từng cặp và thêm phần còn lại
+                    {
+                        for (int j = 0; j < bytes[i].Length; j += 2)
+                        {
+                            convertedBytes.Add(bytes[i].Substring(j, 2));
+                        }
+                    }
+                }
+            }
+            return String.Join(" ", convertedBytes.ToArray());
+        }
+    }
+}
